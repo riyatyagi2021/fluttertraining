301daff503e23da14db300b3729a763fe0763314
riyatyagi2021 <riya.mobcoder@gmail.com>
2021-09-29 16:19:22 +0530

    gridview

--- flutterprojects/lib/pagesdrawer/thirdpage.dart
+++ flutterprojects/lib/pagesdrawer/thirdpage.dart
@@ -10,17 +10,66 @@ class ThirdPage extends StatefulWidget {
 }
 
 class _ThirdPageState extends State<ThirdPage> {
+
+  List<String> images=[
+    'assets/images/gi1.jpeg',
+    'assets/images/gi2.jpeg',
+    'assets/images/gi3.jpeg',
+    'assets/images/gi4.jpeg',
+    'assets/images/gi5.jpeg',
+    'assets/images/gi6.jpeg',
+    'assets/images/wall2.jpeg',
+    'assets/images/hi.jpeg',
+    'assets/images/cl.jpeg',
+    'assets/images/picture1.jpg',
+    'assets/images/gi1.jpeg',
+    'assets/images/gi2.jpeg',
+    'assets/images/gi3.jpeg',
+    'assets/images/gi4.jpeg',
+    'assets/images/gi5.jpeg',
+    'assets/images/gi6.jpeg',
+    'assets/images/wall2.jpeg',
+    'assets/images/hi.jpeg',
+    'assets/images/cl.jpeg',
+    'assets/images/picture1.jpg',
+  ];
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body:  Container(
-          padding: EdgeInsets.all(10),
-          margin: EdgeInsets.symmetric(vertical: 35,horizontal: 5),
-          alignment: Alignment.center,
-          constraints: BoxConstraints.expand(
-              height: 300
-          ),
-          child: imageSlider(context)),
+        child: Column(
+          children: [
+            Container(
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 35,horizontal: 5),
+                alignment: Alignment.center,
+                constraints: BoxConstraints.expand(
+                    height: 300
+                ),
+                child: imageSlider(context)
+            ),
+            Container(
+              height: 300,
+              padding: EdgeInsets.only(left: 10,right: 10),
+              child: GridView.builder(
+                itemCount:images.length ,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    crossAxisSpacing: 4,
+                    mainAxisSpacing: 5
+                  ),
+                  itemBuilder: (BuildContext context, int index){
+              return Image.asset(images[index]);
+              },
+              ),
+            )
+          ],
+        ),
+             ),
     );
 
   }
@@ -33,7 +82,7 @@ Swiper imageSlider(context){
       return Image.asset('assets/images/picture1.jpg',fit: BoxFit.cover,);
     },
     scale: 0.8,
-// viewportFraction: 0.7,
+    // viewportFraction: 0.7,
     pagination: SwiperPagination(),
     control: SwiperControl()
   );
